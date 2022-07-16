274b6c0cb16eb2432eac03851c2adacfe54acb7a
Ilya Chernikov <chernikov.i123@gmail.com>
2022-07-16 19:33:44 +0700

    Changed slider in Judo scene

--- Code/Levels/JudoLevel.cs
+++ Code/Levels/JudoLevel.cs
@@ -14,20 +14,25 @@ public class JudoLevel : Node2D, ILevel
     public Node2D World => _world;
     private Node2D _world;
 
-    private float _greenZoneStartValue; // 0 - 100
-    private float _greenZoneEndValue; // 0 - 100
-    private float _greenZoneSize; // 0 - 100
+    private float _greenZoneStartValue; // 0 - 1
+    private float _greenZoneEndValue; // 0 - 1
+    // private float _qteGrabberNormWidth;
+
+    [Export] private float _qteAcceptableCoverage = 0.75f;
 
     [Export] private float _qteFillDuration = 3.0f; // seconds
     [Export] private float _qteGreenZoneCoverPercentage = 0.05f;
+    [Export] private float _qteYellowZoneCoverPercentage = 0.05f;
 
     private AnimatedSprite _playerCharacter;
     private AnimatedSprite _opponentCharacter;
 
-    private ProgressBar _qteProgressBar;
+    private HSlider _qteBar;
     private ColorRect _qteGreenZone;
+    private ColorRect _qteYellowZone;
     private Button _qteButton;
     private Tween _qteFillTween;
+    private bool _qteCurrentTweenIsLTR = true;
 
     public override void _Ready()
     {
@@ -38,29 +43,62 @@ public class JudoLevel : Node2D, ILevel
         _playerCharacter = GetNode<AnimatedSprite>("Persons/Player/AnimatedSprite");
         _opponentCharacter = GetNode<AnimatedSprite>("Persons/Opponent/AnimatedSprite");
 
-        _qteProgressBar = GetNode<ProgressBar>("QTE/VBoxContainer/ProgressBar");
-        _qteGreenZone = GetNode<ColorRect>("QTE/VBoxContainer/ProgressBar/GreenZone");
-        _qteButton = GetNode<Button>("QTE/VBoxContainer/HBoxContainer/Button");
+        _qteBar = GetNode<HSlider>("QTE/VBoxContainer/HSlider");
+        _qteGreenZone = GetNode<ColorRect>("QTE/VBoxContainer/HSlider/GreenZone");
+        _qteYellowZone = GetNode<ColorRect>("QTE/VBoxContainer/HSlider/YellowZone");
+        _qteButton = GetNode<Button>("QTE/Button");
+
+        // _qteGrabberNormWidth = (float)_qteBarGrabberWidthInPixels / _qteBar.RectSize.x;
 
         ResetUiState();
 
-        _greenZoneSize = _qteGreenZoneCoverPercentage * 100.0f;
-        RandomizeGreenZone();
-        ConfigureGreenZoneWidget();
+        RandomizeGreenZonePosition();
+        ConfigureZoneWidgets();
 
-        _qteFillTween = new Tween();
-        _qteFillTween.InterpolateProperty(_qteProgressBar, "value", 0.0f, 100.0f, _qteFillDuration, Tween.TransitionType.Linear);
-
-        _qteProgressBar.Connect("value_changed", this, nameof(OnQteProgressBarValueChanged));
         _qteButton.Connect("pressed", this, nameof(OnQteButtonPressed));
 
+        _qteFillTween = new Tween();
+        _qteFillTween.Connect("tween_completed", this, nameof(SwapQteBarTweenDirection));
+        _qteFillTween.Connect("tween_step", this, nameof(OnBarTweenStep));
+
         AddChild(_qteFillTween);
-        _qteFillTween.Start();
+
+        _qteCurrentTweenIsLTR = false;
+        SwapQteBarTweenDirection(_qteBar, string.Empty);
+    }
+
+    private void SwapQteBarTweenDirection(Object obj, string path) // path is required for "tween_completed" signal
+    {
+        if (_qteCurrentTweenIsLTR)
+        {
+            _qteFillTween.Stop(obj);
+            _qteFillTween.InterpolateProperty(obj, "value", 100.0f, 0.0f, _qteFillDuration);
+            _qteFillTween.Start();
+            _qteCurrentTweenIsLTR = false;
+        }
+        else
+        {
+            _qteFillTween.Stop(obj);
+            _qteFillTween.InterpolateProperty(obj, "value", 0.0f, 100.0f, _qteFillDuration);
+            _qteFillTween.Start();
+            _qteCurrentTweenIsLTR = true;
+        }
+    }
+
+    private float Remap(float value, float from1, float to1, float from2, float to2)
+    {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+    }
+    
+    private void OnBarTweenStep(Object obj, string path, float elapsed, float value)
+    {
+        float newPosX = Remap(value, 0.0f, 100.0f, 0.0f, _qteBar.RectSize.x);
+        _qteYellowZone.RectPosition = new Vector2(newPosX, 0.0f);
     }
 
     private void ResetUiState()
     {
-        _qteProgressBar.Value = 0;
+        _qteBar.Value = 0;
 
         _playerCharacter.Animation = "Idle";
         _playerCharacter.Playing = true;
@@ -68,36 +106,55 @@ public class JudoLevel : Node2D, ILevel
         _opponentCharacter.Playing = true;
     }
 
-    private void RandomizeGreenZone()
+    private void RandomizeGreenZonePosition()
     {
-        float startValue = GD.Randf() * 100.0f;
-        if (startValue + _greenZoneSize > 100.0f)
-        {
-            startValue = 100.0f - _greenZoneSize;
-        }
+        float startValue = GD.Randf();
+        if (startValue + _qteGreenZoneCoverPercentage > 1.0f)
+            startValue = 1.0f - _qteGreenZoneCoverPercentage;
 
         _greenZoneStartValue = startValue;
-        _greenZoneEndValue = startValue + _greenZoneSize;
+        _greenZoneEndValue = startValue + _qteGreenZoneCoverPercentage;
     }
 
-    private void ConfigureGreenZoneWidget()
+    private void ConfigureZoneWidgets()
     {
         Debug.Assert(_qteGreenZone != null);
 
         _qteGreenZone.RectSize = new Vector2(
-            _qteProgressBar.RectSize.x * _qteGreenZoneCoverPercentage,
-            _qteProgressBar.RectSize.y
+            _qteBar.RectSize.x * _qteGreenZoneCoverPercentage,
+            _qteBar.RectSize.y
         );
 
         _qteGreenZone.RectPosition = new Vector2(
-            _qteProgressBar.RectSize.x * _greenZoneStartValue / 100.0f,
+            _qteBar.RectSize.x * _greenZoneStartValue,
             0
         );
+
+        _qteYellowZone.RectSize = new Vector2(
+            _qteBar.RectSize.x * _qteYellowZoneCoverPercentage,
+            _qteBar.RectSize.y
+        );
+
+        _qteYellowZone.RectPosition = new Vector2(0, 0);
     }
 
     private bool IsValueInGreenZone(float value)
     {
-        return value >= _greenZoneStartValue && value <= _greenZoneEndValue;
+        float valueNorm = value / 100.0f;
+        float minX = valueNorm;
+        float maxX = valueNorm + _qteYellowZoneCoverPercentage;
+
+        if (maxX < _greenZoneStartValue || minX > _greenZoneEndValue)
+        {
+            GD.Print("IsValueInGreenZone(): value is completely outsize");
+            return false;
+        }
+
+        float globalCoverage = Mathf.Min(maxX, _greenZoneEndValue) - Mathf.Max(minX, _greenZoneStartValue);
+        float coverageOfYellow = Mathf.Clamp(globalCoverage / _qteYellowZoneCoverPercentage, 0.0f, 1.0f);
+        GD.Print("IsValueInGreenZone(): coverage: " + coverageOfYellow + "%");
+
+        return coverageOfYellow >= _qteAcceptableCoverage;
     }
 
     public void OnLevelLoad()
@@ -106,20 +163,12 @@ public class JudoLevel : Node2D, ILevel
 
     public void OnLevelUnload()
     {
-    }
-
-    private void OnQteProgressBarValueChanged(float value)
-    {
-        if (value >= 100.0f)
-        {
-            OnQteTimedOut();
-            return;
-        }
+        QueueFree();
     }
 
     private void OnQteButtonPressed()
     {
-        float currentValue = (float) _qteProgressBar.Value;
+        float currentValue = (float) _qteBar.Value;
 
         if (IsValueInGreenZone(currentValue))
             OnQteGoodUserResult();
@@ -148,20 +197,11 @@ public class JudoLevel : Node2D, ILevel
         throwTween.Start();
     }
 
-    private void OnQteTimedOut()
-    {
-        EmitSignal(nameof(OnTimedOut));
-        GD.Print("You lost");
-
-        PlayThrowAnimation(_opponentCharacter, _playerCharacter, -1.0f);
-    }
-
     private void OnQteBadUserResult()
     {
         EmitSignal(nameof(OnBadResult));
-        GD.Print("You pressed not in time :(");
 
-        _qteFillTween.Stop(_qteProgressBar);
+        _qteFillTween.Stop(_qteBar);
 
         PlayThrowAnimation(_opponentCharacter, _playerCharacter, -1.0f);
     }
@@ -169,9 +209,8 @@ public class JudoLevel : Node2D, ILevel
     private void OnQteGoodUserResult()
     {
         EmitSignal(nameof(OnGoodResult));
-        GD.Print("You pressed in time :)");
 
-        _qteFillTween.Stop(_qteProgressBar);
+        _qteFillTween.Stop(_qteBar);
 
         PlayThrowAnimation(_playerCharacter, _opponentCharacter, 1.0f);
     }
